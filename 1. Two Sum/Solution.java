74dd17fda214161d76fa6c81134f0c820b63ce49
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-11 12:56:50 +0100

    removed .class files

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,44 @@
+/*
+ * LeetCode 1. Two Sum
+
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+
+ * https://leetcode.com/problems/two-sum/description/
+
+ */
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // a + b = target
+        int a, b;
+        
+        for (int n : nums) {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example 1: nums = [2,7,11,15], target = 9
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Example 1: [" + result1[0] + "," + result1[1] + "]"); // Expected: [0,1]
+        
+        // Example 2: nums = [3,2,4], target = 6  
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Example 2: [" + result2[0] + "," + result2[1] + "]"); // Expected: [1,2]
+        
+        // Example 3: nums = [3,3], target = 6
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Example 3: [" + result3[0] + "," + result3[1] + "]"); // Expected: [0,1]
+    }
+}
