35afb9018250b495bcbbae1d4b50de930af40161
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-11 12:58:09 +0100

    Updated 474. Ones and Zeroes

--- 474. Ones and Zeroes/Solution.java
+++ 474. Ones and Zeroes/Solution.java
@@ -1,13 +1,13 @@
-/**
- * LeetCode 474. Ones and Zeroes
- * 
+/*
+ * LeetCode problem 474. Ones and Zeroes
+
  * You are given an array of binary strings strs and two integers m and n.
  * Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
- * 
  * A set x is a subset of a set y if all elements of x are also elements of y.
- */
+ 
+ * https://leetcode.com/problems/ones-and-zeroes/description/
 
-// https://leetcode.com/problems/ones-and-zeroes/description/
+ */
 
 public class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
